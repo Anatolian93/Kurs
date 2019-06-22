4f1caffb6fdd81730e00b983c57ed2a5380cd2c4
Tolga Büyükatilla <tolga-atilla93@hotmail.com>
2019-06-22 14:02:11 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,76 @@
+## yoruuuuuuuuuuuuuum
+## 1) degisken adı sayı ile başlayamaz.
+## 2) degisken 2 kelimeden oluşamaz.
+## 3) degisken ismi içerisinde sadece izin verilen özel karakterler kullanılır.(_)
+## 4) degisken, tanımlaması yapılırken tanımlı kelimeler kullanılamaz.
+## 5)değişken, ismi içerisinde türkçe karakter kullanmayınız. 
+## Convert : veri tipini başka değişkene çevirme
+
+
+# var, , string, 
+##
+##sayi = 5
+##print (sayi)
+##print (type(sayi))
+##
+##ondalikli_sayi = 4.6
+##print(ondalikli_sayi)
+##print (type(ondalikli_sayi))
+##
+##isim = " tolga"
+##soyisim =  " büyükatilla\n"
+##
+##print("adım " + isim + " soyadım " + soyisim)
+##
+##fullname = isim + " " + soyisim
+##
+##x= True
+##y= False
+##
+##print (x)
+##print (fullname *5)
+
+
+###########################
+##sayi1 = float(input("Lutfen bir sayi giriniz : "))
+##sayi2 = float(input("Lutfen bir sayi giriniz : "))
+##islem = input("Yapılacak işlemi girin : {Toplama - Çıkarma - Çarpma - Bölme - Mod- FinalHesapla}")
+##
+##if (islem == "Toplama"):
+##    toplami  = sayi1 + sayi2
+##    print (toplami)
+##
+##elif (islem == "Çıkarma"):
+##    fark = sayi1 - sayi2
+##    print (fark)
+##
+##elif (islem == "Çarpma"):
+##    carpim = sayi1 * sayi2
+##    print (carpim)
+##
+##elif (islem == "Bölme"):
+##    sonucu = sayi1 / sayi2
+##    print (sonucu)
+##
+##elif (islem == "Mod"):
+##    modu = sayi1 % sayi2
+##    print (modu)
+##    
+##elif (islem == "FinalHesapla"):
+##    final = sayi1 * 0.30 + sayi2 * 0.70
+##    print (final)
+##
+##else:
+##    print ("WTFFFF bir işlem seç len")
+##
+
+##############################
+
+##print("""bilge
+##adam
+##besiktas""")
+##
+
+ad = str(input("Lutfen kullanıcı adını girin : "))
+soyad = str(input("Lutfen kullanıcı soyadını girin : "))
+print (ad + "." + soyad+ "@hotmail.com")
